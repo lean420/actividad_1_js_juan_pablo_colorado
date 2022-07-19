5b8f9397aedf03d5a00a86cb431c191c10042434
LozanoAndersonTheStain <adslozanov@gmail.com>
2022-07-18 21:15:18 -0500

    commit inicial

---
+++ app.js
@@ -0,0 +1,10 @@
+let name = 'juan pablo', 
+lastname = 'Colorado', 
+yearOfBirth = 2001, 
+cityOfBirth = 'medellin'
+
+console.log(             
+    `Hola, mi nombre es ${name} ${lastname}, vivo en ${cityOfBirth} y tengo ${
+        2022 - yearOfBirth
+             } años.` 
+    )
